80963931d12c047a906412e00ebf41bb0d0fcbd5
alwinW96 <99060433@mydavinci.nl>
2021-09-15 13:37:45 +0200

    input en if-statement

---
+++ meten-is-weten.py
@@ -0,0 +1,8 @@
+a = input('geef een getal hier: ')
+b = input('geef de tweede getal hier: ')
+
+
+if a > b:
+    print('getal'' ' + str(a) + ' ''is groter')
+elif a < b: 
+    print('getal'' ' + str(b) + ' ''is groter')
